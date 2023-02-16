d02e893d0ae397f18b0dec05668d09380d2d0bd4
jluethi <joel.luethi@uzh.ch>
2023-02-16 16:09:39 +0100

    Reset classifier code to 0.0.1 release

--- src/napari_feature_classifier/classifier.py
+++ src/napari_feature_classifier/classifier.py
@@ -1,20 +1,25 @@
 """ Classifier class file """
-import os
-import pickle
 from collections import OrderedDict
 from pathlib import Path
-from typing import Union
-
-import numpy as np
+import pickle
+import os
+import warnings
+from zlib import crc32
 import pandas as pd
+import numpy as np
+from sklearn.metrics import f1_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import f1_score
+from .utils import napari_info
 
-from napari_feature_classifier.data import ClassifierData
-from napari_feature_classifier.utils import napari_info
 
-CompatibleEstimators = Union[RandomForestClassifier, LogisticRegression]
+def make_identifier(df):  # pylint: disable-msg=C0103, C0116
+    str_id = df.apply(lambda x: "_".join(map(str, x)), axis=1)
+    return str_id
+
+
+def test_set_check(identifier, test_ratio):  # pylint: disable-msg=C0116
+    return crc32(np.int64(hash(identifier))) & 0xFFFFFFFF < test_ratio * 2**32
 
 
 def load_classifier(classifier_path):  # pylint: disable-msg=C0116
@@ -79,18 +84,31 @@ class Classifier:
     """
 
     def __init__(
-            self,
-            name: str,
-            data: ClassifierData,
-            training_features: list,
-            clf: CompatibleEstimators = RandomForestClassifier(),
-            directory=Path("."),
+        self,
+        name,
+        features,
+        training_features,
+        method="rfc",
+        directory=Path("."),
+        index_columns=None,
     ):
+        # TODO: Think about changing the not classified class to NaN instead of 0
+        # (when manually using the classifier, a user may provide 0s as training
+        # input when predicting some binary result)
         self.name = name
-        self.data = data
-        self.training_features = training_features
-        self.clf = clf
         self.directory = directory
+        if method == "rfc":
+            self.clf = RandomForestClassifier()
+        elif method == "lrc":
+            self.clf = LogisticRegression()
+        full_data = features
+        full_data.loc[:, "train"] = 0
+        full_data.loc[:, "predict"] = 0
+        self.index_columns = index_columns
+        self.train_data = full_data[["train"]]
+        self.predict_data = full_data[["predict"]]
+        self.training_features = training_features
+        self.data = full_data[self.training_features]
         # TODO: Improve this flag. Currently user needs to set the flag to
         # false when changing training data. How can I automatically change
         # the flag whenever someone modifies self.train_data?
@@ -114,9 +132,74 @@ class Classifier:
         #   one feature is always NaN, the classifier wouldn't do anything anymore
         # 3. Handle booleans: Convert to numeric 0 & 1.
 
-    def add_data(self, data: pd.DataFrame):
-        self.data.append_data_frame(data)
+    @staticmethod
+    def train_test_split(
+        df, test_perc=0.2, index_columns=None
+    ):  # pylint: disable-msg=C0103
+        in_test_set = make_identifier(df.reset_index()[list(index_columns)]).apply(
+            test_set_check, args=(test_perc,)
+        )
+
+        if in_test_set.sum() == 0:
+            warnings.warn(
+                "Not enough training data. No training data was put in the "
+                "test set and classifier will fail."
+            )
+        if in_test_set.sum() == len(in_test_set):
+            warnings.warn(
+                "Not enough training data. All your selections became test "
+                "data and there is nothing to train the classifier on."
+            )
+        return df.iloc[~in_test_set.values, :], df.iloc[in_test_set.values, :]
+
+    def add_data(self, features, training_features, index_columns):
+        # Check that training features agree with already existing training features
+        assert training_features == self.training_features, (
+            "The training "
+            "features provided to the classifier are different to what has "
+            "been used for training so far. This has not been implemented "
+            f"yet. Old vs. new: {self.training_features} vs. {training_features}"
+        )
+
+        # Check if data with the same index already exists. If so, do nothing
+        assert index_columns == self.index_columns, (
+            "The newly added dataframe "
+            "uses different index columns "
+            "than what was used in the "
+            f"classifier before: New {index_columns}, "
+            f"before {self.index_columns}"
+        )
+        # Check which indices already exist in the data, only add the others
+        new_indices = self._index_not_in_other_df(features, self.train_data)
+        new_data = features.loc[new_indices["index_new"]]
+        if len(new_data.index) == 0:
+            # No new data to be added: The classifier is being loaded for a
+            # site where the data has been loaded before
+            # TODO: Is there a low-priority logging this could be sent to?
+            # Not a warning, just info or debug
+            pass
+        else:
+            new_data["train"] = 0
+            new_data["predict"] = 0
+            # self.train_data = self.train_data.append(new_data[["train"]])
+            # self.predict_data = self.predict_data.append(new_data[["predict"]])
+            # self.data = self.data.append(new_data[training_features])
+            self.train_data = pd.concat([self.train_data, new_data[["train"]]])
+            self.predict_data = pd.concat([self.predict_data, new_data[["predict"]]])
+            self.data = pd.concat([self.data, new_data[training_features]])
         self.is_trained = False
+
+    @staticmethod
+    def _index_not_in_other_df(df1, df2):
+        # Function checks which indices of df1 already exist in the indices of df2.
+        # Returns a boolean pd.DataFrame with a 'index_preexists' column
+        df_overlap = pd.DataFrame(index=df1.index)
+        for df1_index in df1.index:
+            if df1_index in df2.index:
+                df_overlap.loc[df1_index, "index_new"] = False
+            else:
+                df_overlap.loc[df1_index, "index_new"] = True
+        return df_overlap
 
     @staticmethod
     def get_non_na_indices(df, message=""):  # pylint: disable-msg=C0103
@@ -145,36 +228,49 @@ class Classifier:
         return non_nan_indices
 
     def train(self, ignore_nans=True):
+        # TODO: Select training data differently. 0 could be a valid training input
+        # Load only training features. The data df will also contain the prior
+        # predictions, which can lead to issues
         self.is_trained = True
-        training_data = self.data.training_data
-        training_annotations = self.data.training_annotations
-
-        test_data = self.data.test_data
-        test_annotations = self.data.test_annotations
+        training_data = self.data.loc[
+            self.train_data["train"] > 0, self.training_features
+        ]
+        training_results = self.train_data[self.train_data["train"] > 0]
 
         if ignore_nans:
-            non_nan_training_indices = self.get_non_na_indices(training_data, message="training")
-            non_nan_test_indices = self.get_non_na_indices(test_data, message="test")
+            non_nan_indices = self.get_non_na_indices(training_data, message="training")
+            # pylint: disable-msg=C0103
+            X_train, X_test = self.train_test_split(
+                training_data[non_nan_indices], index_columns=self.index_columns
+            )
+            y_train, y_test = self.train_test_split(
+                training_results[non_nan_indices], index_columns=self.index_columns
+            )
+        else:
+            X_train, X_test = self.train_test_split(  # pylint: disable-msg=C0103
+                training_data, index_columns=self.index_columns
+            )
+            y_train, y_test = self.train_test_split(
+                training_results, index_columns=self.index_columns
+            )
 
-            training_data = training_data.loc[non_nan_training_indices.values, :]
-            training_annotations = training_annotations.loc[non_nan_training_indices.values, :]
-            test_data = test_data.loc[non_nan_test_indices.values, :]
-            test_annotations = test_annotations.loc[non_nan_test_indices.values, :]
+        assert np.all(X_train.index == y_train.index)
+        assert np.all(X_test.index == y_test.index)
 
-        self.clf.fit(training_data, training_annotations.astype(int))
+        self.clf.fit(X_train, y_train["train"])
 
         # pylint: disable-msg=C0103
-        f1 = f1_score(test_annotations.astype(int), self.clf.predict(test_data), average="macro")
+        f1 = f1_score(y_test, self.clf.predict(X_test), average="macro")
         # napari_info("F1 score on test set: {}".format(f1))
         napari_info(
             f"F1 score on test set: {f1} \n"
-            f"Annotations split into {len(training_data)} training and {len(test_data)} "
+            f"Annotations split into {len(X_train)} training and {len(X_test)} "
             "test samples. \n"
-            f"Training set contains {self.get_count_per_class(training_annotations)}. \n"
-            f"Test set contains {self.get_count_per_class(test_annotations)}."
+            f"Training set contains {self.get_count_per_class(y_train['train'])}. \n"
+            f"Test set contains {self.get_count_per_class(y_test['train'])}."
         )
-        self.data.predictions.loc[:] = self.predict(
-            self.data.features, ignore_nans=ignore_nans
+        self.predict_data.loc[:] = self.predict(
+            self.data, ignore_nans=ignore_nans
         ).reshape(-1, 1)
         return f1
 
@@ -206,19 +302,19 @@ class Classifier:
     def predict(self, data, ignore_nans=True):
         if not self.is_trained:
             self.train()
-        if ignore_nans:  # pylint: disable-msg=R1705
+        if ignore_nans: # pylint: disable-msg=R1705
             # Does not throw an exception if data contains a NaN
             # Just returns NaN as a result for any cell containing NaNs
             non_nan = self.get_non_na_indices(
-                data.loc[:, self.data.training_feature_names], message="prediction"
+                data.loc[:, self.training_features], message="prediction"
             )
             data.loc[:, "predict"] = np.nan
             data.loc[non_nan, "predict"] = self.clf.predict(
-                data.loc[non_nan, self.data.training_feature_names]
+                data.loc[non_nan, self.training_features]
             )
             return np.array(data["predict"])
         else:
-            return self.clf.predict(data.loc[:, self.data.training_feature_names])
+            return self.clf.predict(data.loc[:, self.training_features])
 
     def export_results(self, export_path):
         # Run the training & predictions on the current data if any new data
@@ -226,7 +322,7 @@ class Classifier:
         if not self.is_trained:
             self.train()
         # Merge prediction data and training data
-        output_df = pd.merge(self.data.predictions, self.data.annotations, on=self.data.index_columns)
+        output_df = pd.merge(self.predict_data, self.train_data, on=self.index_columns)
         output_df.to_csv(export_path)
 
     def feature_importance(self):
@@ -235,15 +331,15 @@ class Classifier:
                 {
                     f: i
                     for f, i in zip(
-                    self.training_features, self.clf.feature_importances_
-                )
+                        self.training_features, self.clf.feature_importances_
+                    )
                 }.items(),
                 key=lambda x: x[1],
                 reverse=True,
             )
         )
 
-    def most_important(self, n=5):  # pylint: disable-msg=C0103
+    def most_important(self, n=5): # pylint: disable-msg=C0103
         return list(self.feature_importance().keys())[:n]
 
     def save(self, new_name=None, directory=None):
