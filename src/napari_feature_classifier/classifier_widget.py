f73b2fa0a79e3614f0c97054c55f5fbc4dae2ec0
MaksHess <max.hess@imls.uzh.ch>
2023-03-10 13:35:54 +0100

    dev stuff

--- src/napari_feature_classifier/classifier_widget.py
+++ src/napari_feature_classifier/classifier_widget.py
@@ -2,6 +2,7 @@ import math
 import warnings
 from enum import Enum
 from functools import partial
+from pathlib import Path
 from typing import Optional, Sequence, cast
 
 import imageio
@@ -31,28 +32,28 @@ from napari_feature_classifier.annotator_widget import (
 
 
 def main():
-    lbls = imageio.imread(
-        r"C:\Users\hessm\Documents\Programming\Python\fractal-napari-plugins\napari-feature-classifier\src\napari_feature_classifier\sample_data\test_labels.tif"
-    )
+    lbls = imageio.imread(Path("sample_data/test_labels.tif"))
+    labels = np.unique(lbls)[1:]
+
     viewer = napari.Viewer()
     lbls_layer = viewer.add_labels(lbls)
-    labels = np.unique(lbls)[1:]
-    rng = np.random.default_rng(seed=42)
-    n_features = 10
-    features = rng.random(size=(len(labels), n_features))
-    print(lbls_layer.features)
-    # viewer.show(block=True)
+    lbls_layer.features = get_features(labels, n_features=6)
     widget = ClassifierWidget(viewer)
     viewer.window.add_dock_widget(widget)
     viewer.show(block=True)
 
 
+def get_features(labels: Sequence[int], n_features: int = 10, seed: int = 42):
+    columns = [f"feature_{i}" for i in range(n_features)]
+    rng = np.random.default_rng(seed=seed)
+    features = rng.random(size=(len(labels), n_features))
+    return pd.DataFrame(index=labels, columns=columns, data=features)
+
+
 class ClassifierInitContainer(Container):
-    def __init__(self):
+    def __init__(self, feature_options: list[str]):
         self._name_edit = LineEdit(value="classifier")
-        self._feature_combobox = Select(
-            choices=self.get_feature_options(), allow_multiple=True
-        )
+        self._feature_combobox = Select(choices=feature_options, allow_multiple=True)
         self._annotation_name_selector = LabelAnnotatorTextSelector()
         self._initialize_button = PushButton(text="Initialize")
         super().__init__(
@@ -63,9 +64,6 @@ class ClassifierInitContainer(Container):
                 self._initialize_button,
             ]
         )
-
-    def get_feature_options(self):
-        return [f"feature_{i}" for i in range(10)]
 
 
 class ClassifierRunContainer(Container):
@@ -82,10 +80,23 @@ class ClassifierRunContainer(Container):
         )
 
 
+class LoadFeaturesContainer(Container):
+    pass
+
+
+class LoadClassifierContainer(Container):
+    pass
+
+
 class ClassifierWidget(Container):
     def __init__(self, viewer: napari.viewer.Viewer):
         self._viewer = viewer
-        self._init_container = ClassifierInitContainer()
+        # Extract feature for first label layer
+        label_layer = [
+            l for l in self._viewer.layers if isinstance(l, napari.layers.Labels)
+        ][0]
+        feature_names = list(label_layer.features.columns)
+        self._init_container = ClassifierInitContainer(feature_names)
         print(type(self._init_container))
         super().__init__(widgets=[self._init_container])
         self._init_container._initialize_button.clicked.connect(
@@ -93,11 +104,11 @@ class ClassifierWidget(Container):
         )
 
     def initialize_run_widget(self):
-         class_names = self._init_container._annotation_name_selector.get_class_names()
-         # FIXME: Parse selected features
-         # FIXME: Parse selected name
-         self.close()
-         # self.runner_container = ClassifierRunContainer(self.viewer, class_names)
+        class_names = self._init_container._annotation_name_selector.get_class_names()
+        # FIXME: Parse selected features
+        # FIXME: Parse selected name
+        self.close()
+        # self.runner_container = ClassifierRunContainer(self.viewer, class_names)
 
 
 if __name__ == "__main__":
