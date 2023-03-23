c0fc47ea41671c468593d6813f9de963d3807c5e
MaksHess <max.hess@imls.uzh.ch>
2023-03-23 12:01:36 +0100

    index as series instead of index.

--- src/napari_feature_classifier/feature_loader_widget.py
+++ src/napari_feature_classifier/feature_loader_widget.py
@@ -8,50 +8,58 @@ import pandas as pd
 import pandera as pa
 from magicgui.widgets import Container, FileEdit, PushButton
 from napari.utils.notifications import show_info
-from pandera.typing import DataFrame, Index
+from pandera.typing import DataFrame, Index, Series
 
 
 class LabelFeatureSchema(pa.SchemaModel):
-    label: Index[int] = pa.Field(coerce=True, unique=True, check_name=False)
+    roi_id: Series[str] = pa.Field(coerce=True, unique=False)
+    label: Series[int] = pa.Field(coerce=True, unique=True)
 
 
 @pa.check_types
-def load_features(fn: PathLike) -> DataFrame[LabelFeatureSchema]:
+def load_features_csv(
+    fn: PathLike, index_column="label"
+) -> DataFrame[LabelFeatureSchema]:
+    df = pd.read_csv(fn).set_index(index_column)
     # return pd.DataFrame(data=np.random.rand(5, 5), columns=list("abcde"), index=list(range(1, 10, 2)))
-    return DataFrame[LabelFeatureSchema](
-        data=np.random.rand(5, 5), columns=list("abcde"), index=list(range(5))
-    )
+    return DataFrame[LabelFeatureSchema](df)
 
 
 @pa.check_types
 def make_features(
-    labels: Sequence[int], n_features: int = 10, seed: int = 42
+    labels: Sequence[int], roi_id: str='id', n_features: int = 10, seed: int = 42
 ) -> DataFrame[LabelFeatureSchema]:
     columns = [f"feature_{i}" for i in range(n_features)]
     rng = np.random.default_rng(seed=seed)
     features = rng.random(size=(len(labels), n_features))
-    return DataFrame[LabelFeatureSchema](index=labels, columns=columns, data=features)
+    data = {
+        **{"roi_id": roi_id, "label": labels},
+        **{column: feature for column, feature in zip(columns, features.T)},
+    }
+    return DataFrame[LabelFeatureSchema](data)
 
 
 FeatureLoaderFn: TypeAlias = Callable[[PathLike[str]], DataFrame[LabelFeatureSchema]]
 
 
 class LoadFeaturesContainer(Container):
-    def __init__(self, viewer: napari.viewer.Viewer):
-        self._viewer = viewer
+    def __init__(
+        self,
+        labels_layer: napari.viewer.Viewer,
+        loader: FeatureLoaderFn = load_features_csv,
+    ):
+        self._labels_layer = labels_layer
+        self._loader = loader
         self._load_destination = FileEdit(value="sample_data/test_df.csv", mode="r")
         self._load_button = PushButton(label="Load Features")
         super().__init__(widgets=[self._load_destination, self._load_button])
         self._load_button.clicked.connect(self.load)
 
     def load(self):
-        fn = self._load_destination.value
-        df = pd.read_csv(fn).set_index("label")
-        if self._label_layer.features.index.empty:
-            self._label_layer.features.index = np.unique(self._label_layer.data)[1:]
-        self._label_layer.features = self._label_layer.features.join(df, how="left")
         show_info("loading csv...")
+        fn = self._load_destination.value
+        df = self._loader(fn)
+        if self._labels_layer.features.index.empty:
+            self._labels_layer.features.index = np.unique(self._label_layer.data)[1:]
+        self._label_layer.features = self._label_layer.features.join(df, how="left")
         print(self._label_layer.features)
-
-
-# %%
