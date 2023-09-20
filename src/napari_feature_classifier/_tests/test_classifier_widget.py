7d81dadcee33cd72d4ae9d944b60e8ff0b2cbc62
jluethi <joel.luethi@uzh.ch>
2023-09-20 22:43:18 +0200

    Add tests for classifier widget

--- src/napari_feature_classifier/_tests/test_classifier_widget.py
+++ src/napari_feature_classifier/_tests/test_classifier_widget.py
@@ -14,8 +14,13 @@ from napari_feature_classifier.classifier_widget import (
     LoadClassifierContainer,
 )
 
-lbl_img_np = imageio.v2.imread(Path("src/napari_feature_classifier/sample_data/test_labels.tif"))
-clf_path = Path("src/napari_feature_classifier/sample_data/test_labels_classifier.clf")
+lbl_img_np = imageio.v2.imread(
+    Path("src/napari_feature_classifier/sample_data/test_labels.tif")
+)
+clf_path = Path(
+    "src/napari_feature_classifier/sample_data/" "test_labels_classifier.clf"
+)
+
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
 # capsys is a pytest fixture that captures stdout and stderr output streams
@@ -32,13 +37,21 @@ def test_classifier_widgets_initialization_no_features_selected(make_napari_view
 
     classifier_widget.initialize_run_widget()
 
-    # TODO: Catch that it doesn't actually initialize, but instead shows a 
+    # TODO: Catch that it doesn't actually initialize, but instead shows a
     # message to the user that now features were selected
     assert classifier_widget._run_container is None
 
 
+def test_classifier_initializtion_without_label_image(make_napari_viewer):
+    viewer = make_napari_viewer()
+    classifier_widget = ClassifierWidget(viewer)
+    assert classifier_widget._init_container._last_selected_label_layer is None
+
+
 features = make_features(np.unique(lbl_img_np)[1:], roi_id="ROI1", n_features=6)
 features_no_roi_id = features.drop(columns=["roi_id"])
+
+
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
 @pytest.mark.parametrize("features", [features, features_no_roi_id])
 def test_running_classification_through_widget(features, make_napari_viewer):
@@ -84,8 +97,47 @@ def test_running_classification_through_widget(features, make_napari_viewer):
     # Delete the classifier file (cleanup to avoid overwriting confirmation)
     os.remove("lbl_img_np_classifier.clf")
 
+
 # TODO: Add a test to check the overwrite confirmations working correctly
 # For classifier files, for annotations and for exported predictions
+
+
+features = make_features(np.unique(lbl_img_np)[1:], roi_id="ROI1", n_features=6)
+
+
+# make_napari_viewer is a pytest fixture that returns a napari viewer object
+def test_classifier_fails_running_without_annotation(make_napari_viewer, capsys):
+    """
+    Tests if the main widget launches
+    """
+    # make viewer and add an image layer using our fixture
+    viewer = make_napari_viewer()
+    label_layer = viewer.add_labels(lbl_img_np)
+    label_layer.features = features
+
+    # Start init widget
+    classifier_widget = ClassifierWidget(viewer)
+
+    # Select relevant features
+    classifier_widget._init_container._feature_combobox.value = [
+        "feature_1",
+        "feature_2",
+        "feature_3",
+    ]
+
+    classifier_widget.initialize_run_widget()
+
+    # Run the classifier
+    classifier_widget._run_container.run()
+
+    expected_message = (
+        "INFO: Training failed. A typical reason are not "
+        "having enough annotations. \nThe error message was: Found array "
+        "with 0 sample(s) (shape=(0, 3)) while a minimum of 1 is required "
+        "by RandomForestClassifier."
+    )
+    assert expected_message in capsys.readouterr().out
+
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
 # capsys is a pytest fixture that captures stdout and stderr output streams
@@ -108,15 +160,12 @@ def test_load_classifier_widget(make_napari_viewer, capsys):
     assert loading_widget._run_container._prediction_layer.visible
     assert "prediction" in label_layer.features.columns
 
+
 def test_change_of_loader_filter(make_napari_viewer, capsys):
     viewer = make_napari_viewer()
     loading_widget = LoadClassifierContainer(viewer)
     assert loading_widget._filter.value == "*.clf"
-    assert loading_widget._clf_destination.filter == None
+    assert loading_widget._clf_destination.filter is None
     loading_widget._filter.value = "*.pkl"
     assert loading_widget._clf_destination.filter == "*.pkl"
     assert loading_widget._filter.value == "*.pkl"
-
-
-
-
